e8f724afa2e6de99fc238df8b916940c0be7fb71
thatonevietnamese <taogavail@gmail.com>
2026-05-30 19:19:04 +0700

    Update and rename simple spy.lua to infinity yield + simple spy.lua

---
+++ infinity yield + simple spy.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpySource.lua"))()
