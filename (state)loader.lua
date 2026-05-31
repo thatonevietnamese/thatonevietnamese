fcf13b09dc5e3512d455e8230ab7de82235df7b3
thatonevietnamese <taogavail@gmail.com>
2026-05-31 09:11:38 +0700

    Update and rename loader.lua to (state)loader.lua

---
+++ (state)loader.lua
@@ -0,0 +1,5 @@
+local State = loadstring(game:HttpGet("https://raw.githubusercontent.com/thatonevietnamese/thatonevietnamese/refs/heads/main/(state)current%20state.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thatonevietnamese/thatonevietnamese/refs/heads/main/(state)UI%20ch%C3%ADnh.lua"))()(State)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thatonevietnamese/thatonevietnamese/refs/heads/main/(state)auto%20star%20and%20fruits.lua"))()(State)
--- loader.lua
+++
@@ -1,5 +0,0 @@
-local State = loadstring(game:HttpGet("https://raw.githubusercontent.com/thatonevietnamese/thatonevietnamese/refs/heads/main/current%20state.lua"))()
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/thatonevietnamese/thatonevietnamese/refs/heads/main/UI%20ch%C3%ADnh.lua"))()(State)
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/thatonevietnamese/thatonevietnamese/refs/heads/main/auto%20star%20and%20fruits.lua"))()(State)
