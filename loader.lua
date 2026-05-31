8ae7e87d4729e00175aa4ff197fbc542480ecd8e
thatonevietnamese <taogavail@gmail.com>
2026-05-31 07:50:52 +0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,5 @@
+local State = loadstring(game:HttpGet("https://raw.githubusercontent.com/thatonevietnamese/thatonevietnamese/refs/heads/main/current%20state.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thatonevietnamese/thatonevietnamese/refs/heads/main/UI%20ch%E1%BB%ADnh.lua"))()(State)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thatonevietnamese/thatonevietnamese/refs/heads/main/auto%20star%20and%20fruits.lua"))()(State)
