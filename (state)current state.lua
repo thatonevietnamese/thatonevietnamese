778d2458c0c82061232b51726eb130b81e8d831f
thatonevietnamese <taogavail@gmail.com>
2026-05-31 09:09:37 +0700

    Update and rename current state.lua to (state)current state.lua

--- (state)current state.lua
+++ (state)current state.lua
@@ -1,8 +1,7 @@
--- current state.lua (safe, no HTTP, just returns shared state)
 return {
     BotActive = false,
     SmartJump = true,
-    BlacklistExpiry = 8,
+   BlacklistExpiry = 8,
     ScanTick = 0.25,
     WaitWhenEmpty = 1,
     MoveToTimeout = 1.5,
