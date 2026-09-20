cd63c53b9b322209de1daa960370b975b918de63
thatonevietnamese <taogavail@gmail.com>
2026-09-20 20:43:20 +0700

    Delete infinity yield + simple spy.lua

--- infinity yield + simple spy.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpySource.lua"))()
